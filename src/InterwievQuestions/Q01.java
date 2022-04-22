6b0dbc50b8fb82ed2e305379d1ba857aee87fa2f
DilekBalin <dilekkara1231@gmail.com>
2022-04-22 13:28:31 +0100

    22/04/2022

---
+++ src/InterwievQuestions/Q01.java
@@ -0,0 +1,41 @@
+package InterwievQuestions;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q01 {
+    public static void main(String[] args) {
+        /*
+        Kullanicidan bir String aliniz
+        String'te var olan her karakterin sayisini ekrana yazdiriniz
+        Ornek:
+       alacan>>>> a=3, l=1, c=1, n=1
+       abaa===>  a=3,  b=1
+   */
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen bir String giriniz");
+        String str = scan.nextLine();
+
+        //split()
+        String arr[] = str.split("");  //Data type'i String olan bir array olusturduk ve herbir karakteri parcalaadik
+        Arrays.sort(arr); // tum karakterleri alfabetik sirayla verir
+        System.out.println(Arrays.toString(arr));
+
+        int counter = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1].equals(arr[i])) {
+                counter++;
+            } else {
+                System.out.println(arr[1 - 1] + " sayisi " + (counter + 1) + " kez kullanilmistir");
+                counter = 0;  //sayaci sifirlammaiz gerekir ki diger karakteri de saymya gecsin
+            }
+
+            if (i == arr.length - 1) { ///en sona geldiginde
+                System.out.println(arr[i] + " sayisi " + (counter + 1) + " kez kullanilmistir");
+            }
+
+
+        }
+
+    }}
